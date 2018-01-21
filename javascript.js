3e0721f90b0a7d10dd5db2bf1dcd9b41abd9f99f
grantcarey <gcc216@gmail.com>
2018-01-21 15:03:03 -0500

    Java

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 console.log('Hello World');
-alert('Steelers SUCK!');
+// alert('Steelers SUCK!');
 document.write('Buy the All New M3 Driver!');
-  var numberOfKittens = 5;
+  var numberOfKittens = 10;
+  var numberOfPuppies = 24;
+  var numberOfAnimals = numberOfKittens + numberOfPuppies
 console.log(numberOfKittens);
+console.log(numberOfAnimals);
+var kittensName = 'Jimbo'
+console.log(kittensName);
+console.log('Lary is better than' + kittensName)
